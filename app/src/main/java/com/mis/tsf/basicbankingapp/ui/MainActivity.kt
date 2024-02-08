461e51f8b396aee531c9835b7162b80b574e9110
Mahmoud-Ibrahim-750 <104442642+Mahmoud-Ibrahim-750@users.noreply.github.com>
2024-02-08 13:32:15 +0200

    Initial Commit
    - Empty Project.

---
+++ app/src/main/java/com/mis/tsf/basicbankingapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mis.tsf.basicbankingapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mis.tsf.basicbankingapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
